71f86e4ce3a0b5258f7faba85762d4fb840043c2
wedyaan aloufi <wedyan-14@hotmail.com>
2023-08-19 01:10:34 +0300

    done 2 functions

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
- 
+let Like = document.getElementById("like");
+let checkB = document.getElementById("btnCheck");
+let inputUrl = document.getElementById("inputUrl");
+let addUrl = document.getElementById("addUrl");
+
+// Like Function
+function addLike() {
+  if (Like.style.color == "white") {
+    Like.style.color = "red";
+  } else {
+    Like.style.color = "white";
+  }
+}
+
+//Check Function
+checkB.addEventListener("click", () => {
+  let url = inputUrl.value;
+
+  if (url == "") {
+    addUrl.innerHTML = "Please Enter a URL to Check!";
+    addUrl.style.color = "red";
+  }
+});
