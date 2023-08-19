fcb952a98d3e01cab74c880c3543538064e69ac9
wedyaan aloufi <wedyan-14@hotmail.com>
2023-08-19 22:07:55 +0300

    test

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ let Like = document.getElementById("like");
 let checkB = document.getElementById("btnCheck");
 let inputUrl = document.getElementById("inputUrl");
 let addUrl = document.getElementById("addUrl");
+let choose1 = document.getElementById("choose1");
+let choose2 = document.getElementById("choose2");
+let choose3 = document.getElementById("choose3");
+let choose4 = document.getElementById("choose4");
 
 // Like Function
 function addLike() {
@@ -20,4 +24,21 @@ checkB.addEventListener("click", () => {
     addUrl.innerHTML = "Please Enter a URL to Check!";
     addUrl.style.color = "red";
   }
+});
+addUrl.value = "";
+
+choose1.addEventListener("click", function () {
+  window.open(inputUrl.value, "_blank", "width=375,height=812");
+});
+
+choose2.addEventListener("click", function () {
+  window.open(inputUrl.value, "_blank", "width=412,height=869");
+});
+
+choose3.addEventListener("click", function () {
+  window.open(inputUrl.value, "_blank", "width=1024,height=1366");
+});
+
+choose4.addEventListener("click", function () {
+  window.open(inputUrl.value, "_blank", "width=1920,height=1080");
 });
